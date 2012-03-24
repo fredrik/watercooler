7a5bb25c9572bc35f93a4a673f833587df5add9d
Simon Johansson <simon@editd.com>
2012-03-24 18:14:01 +0000

    Send an email to all users in the system

--- email/email_send.py
+++ email/email_send.py
@@ -1,11 +1,16 @@
 import smtplib  
 import settings
+import os
+os.environ['WATERCOOLER_ENVIRONMENT'] = 'test'
+
+from watercooler.hipflask import environment
+from watercooler.hipflask.models import User
 
 server = smtplib.SMTP('smtp.gmail.com:587')  
 server.starttls()  
 server.login(settings.EMAIL_USER, settings.EMAIL_PASSWORD)  
 
-def go(emails):
+def send_batch(emails):
     message = "From: %s\r\nTo: %s\r\nSubject: Hi! How are you today?\r\n\r\n"
     message += "Hello there %s!\nIts a lovley(rainy/cloudy etc, get from api?) day today and Im curious to know how you are feeling and what you are working on. Can you be a sport and send me back a email?\n\n\nExample email:\n\nI'm feeling glad\nI'm working on stuff today\n\n(If you are working on something with a friend do tell!) \nI'm working on some cool stuff today with @someone\n\nHave a good day!"
     
@@ -13,6 +18,11 @@ def go(emails):
         server.sendmail(settings.EMAIL_USER, email_to, message % (settings.EMAIL_USER, email_to, person))
     server.quit()
 
+def get_all_users():
+    for user in User.objects.all():
+        name = '%s %s' % (user.first_name, user.last_name)
+        email = user.email
+        yield (name, email)
+
 if __name__ == '__main__':
-    emails = [('Simon Johansson', settings.EMAIL_USER)]
-    go(emails)
+    send_batch(get_all_users())
