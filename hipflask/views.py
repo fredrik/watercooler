246fa125eb99a27edb7ab3de4ce54615ebdf6c79
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2012-03-24 14:29:01 +0000

    introduce /status

--- hipflask/views.py
+++ hipflask/views.py
@@ -1,8 +1,22 @@
 from watercooler.hipflask import app
+from models import User, Status
 from flask import render_template
 
 
 @app.route("/")
 def hello():
     return render_template('browser.html')
-    
+
+
+@app.route("/status")
+def status():
+    """
+    Return the latest status for each user.
+    """
+    statuses = []
+    for user in User.objects:
+        latest = Status.objects(user=user).first()
+        if latest:
+            statuses.append(latest)
+
+    return render_template('status.html', statuses=statuses)
