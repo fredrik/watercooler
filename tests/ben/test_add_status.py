955d1afda3fe93a8130e3fa7e0ab40be2c8cf828
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2012-03-24 15:21:17 +0000

    ben: test_add_status_with_emotion()

--- tests/ben/test_add_status.py
+++ tests/ben/test_add_status.py
@@ -5,7 +5,7 @@ os.environ['WATERCOOLER_ENVIRONMENT'] = 'test'
 
 from watercooler.ben.api import Api
 from watercooler.hipflask import environment
-from watercooler.hipflask.models import User, Status
+from watercooler.hipflask.models import User, Status, Emotion
 
 
 class TestAddStatus(unittest2.TestCase):
@@ -20,6 +20,9 @@ class TestAddStatus(unittest2.TestCase):
         # create test fixtures.
         self.user = User(first_name='Lewpen', last_name='Trippin', email='lewpen@corp.com', username='lewpen')
         self.user.save()
+
+        self.sad_emotion = Emotion(name='sad')
+        self.sad_emotion.save()
 
 
     def test_add_status(self):
@@ -36,6 +39,23 @@ class TestAddStatus(unittest2.TestCase):
         self.assertEqual(status.status, 'I was wanting you to love me.')
         self.assertEqual(status.user, self.user)
         self.assertEqual(status.emotion, None)
+
+    def test_add_status_with_emotion(self):
+        """Add a status with an emotion."""
+
+        self.assertEqual([], list(Status.objects))
+
+        api = Api()
+        api.add_status(status='I was wanting you to love me.', user=self.user, emotion=self.sad_emotion)
+
+        # verify that the status was inserted successfully.
+        status = Status.objects.first()
+        self.assertIsNotNone(status)
+        self.assertEqual(status.status, 'I was wanting you to love me.')
+        self.assertEqual(status.user, self.user)
+        self.assertEqual(status.emotion, self.sad_emotion)
+
+
 
 
 
